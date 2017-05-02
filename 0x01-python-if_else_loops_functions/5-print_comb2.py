c6f1a80021a9871ee38870acbcfd2819159805db
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-02 21:55:53 +0000

    fix leading zeros

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num < 10:
-        print("0{:d},".format(num), end=" ")
+        print("{:02d},".format(num), end=" ")
     elif num < 99:
         print("{:d},".format(num), end=" ")
     else:
