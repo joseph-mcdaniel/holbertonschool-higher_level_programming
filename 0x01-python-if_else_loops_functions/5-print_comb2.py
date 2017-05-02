c9839e5ead98d8f77f4edf2006a80e94ad5a0a81
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-02 21:57:10 +0000

    fix spaces

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num < 10:
-        print("{:02d},".format(num), end=" ")
+        print("{:02d}, ".format(num), end="")
     elif num < 99:
-        print("{:d},".format(num), end=" ")
+        print("{:d}, ".format(num), end="")
     else:
         print("{:d}".format(num))
