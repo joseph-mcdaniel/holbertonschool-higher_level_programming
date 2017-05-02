9fee9f2779b75a71e31d8fce50aa80f7f5e98692
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-02 21:53:17 +0000

    print numbers from 00 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for num in range(0, 100):
+    if num < 10:
+        print("0{:d},".format(num), end=" ")
+    elif num < 99:
+        print("{:d},".format(num), end=" ")
+    else:
+        print("{:d}".format(num))
