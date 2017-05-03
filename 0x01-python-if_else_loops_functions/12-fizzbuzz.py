f91004af7d2a6642edbcffe6ee6294d7a43e9374
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-03 06:29:08 +0000

    change format

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("Fizz ", end="")
+            print("{} ".format("Fizz"), end="")
         elif i % 5 == 0:
-            print("Buzz ", end="")
+            print("{} ".format("Buzz"), end="")
         elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz ", end="")
+            print("{} ".format("FizzBuzz"), end="")
         else:
             print("{:d} ".format(i), end="")
