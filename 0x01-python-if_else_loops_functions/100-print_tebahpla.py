da877fe48f1361ca47c1ba4034d5c8233b213e57
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-03 20:47:35 +0000

    remove print function

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for reverse in range(122, 96, -1):
-    if reverse % 2 == 0:
-        print(chr(reverse), end="")
-    else:
-        print((chr(reverse - 32)), end="")
+    if reverse % 2 != 0:
+        reverse = reverse - 32
+    print((chr(reverse)), end="")
