44fbe91e0e44eb7fe62daebd5d35bec961cbd004
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-03 20:54:44 +0000

    format string

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for reverse in range(122, 96, -1):
     if reverse % 2 != 0:
-        reverse = reverse - 32
-    print((chr(reverse)), end="")
+        reverse -= 32
+    print("{}".format(chr(reverse)), end="")
