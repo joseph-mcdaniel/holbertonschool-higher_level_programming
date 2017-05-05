c66d05f42f0b938bb9f6a2ca085dbc3736deac12
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-05 06:24:26 +0000

    print and list args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0 argument.")
+
+    elif len(argv) == 2:
+        print("1 argument:\n1: {:s}".format(argv[1]))
+
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in argv:
+            if len(argv.index(i)) > 0:
+                print("{0}: {1}".format(argv.index(i), i))
