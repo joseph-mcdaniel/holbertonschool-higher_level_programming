ab4f5f2ef47e92c7d91408bbfcfb21a235cce0b8
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-10 23:00:28 +0000

    change condition

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,11 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if my_list == []:
         return None
-    if idx == 0:
-        return None
-    if len(my_list) < 0 or len(my_list) > 5:
-        return None
-    if idx > len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return None
     del my_list[idx]
     return my_list
