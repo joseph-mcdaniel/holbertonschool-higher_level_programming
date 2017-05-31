2d5d47bdb7e5f5906cd0601251a6fdc5753f3da1
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-31 06:03:57 +0000

    add __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        # return area of size * size
+        return self.__size * self.__size
+
+    def __str__(self):
+        # return string format to be printed
+        return "[Square] {}/{}".format(self.__size, self.__size)
