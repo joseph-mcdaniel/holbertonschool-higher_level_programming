ea0cf89b15c6162aca15061b165ee041f4fc4d3f
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-31 06:00:24 +0000

    add new class that inherits Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("self", size)
+        super().__init__(size, size)
+
+    def area(self):
+        # return area of size * size
+        return self.__size * self.__size
