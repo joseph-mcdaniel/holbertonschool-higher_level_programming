82961c00a2aceb6466de2d3b1da3cc13f511cc60
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-05-31 06:16:56 +0000

    add docstrings

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 class MyList(list):
+    """
+    class = MyList
+    """
     def print_sorted(self):
+        """
+        method def = print_sorted
+        """
         # print sorted list
         print(sorted(self))
