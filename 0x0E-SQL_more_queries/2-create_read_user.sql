b5b759f6af5076e14840a1b2ce06524aaf14585d
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-07-18 04:10:37 +0000

    initial commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates database and user
+-- user has select privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- create table (id, name)
+-- name can't be null
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
