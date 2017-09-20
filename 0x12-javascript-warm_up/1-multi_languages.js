630d47c1fd545f04abe8c4530cd34be72239b68c
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-09-20 00:53:43 +0000

    print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// print 3 lines using const
+const C = 'C is fun';
+const Python = 'Python is cool';
+const JS = 'Javascript is amazing';
+console.log(C);
+console.log(Python);
+console.log(JS);
