fa92cf2d0618ff351390a3208c50f22c37e214ba
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-12-31 01:06:29 +0000

    add docstrings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-class Rectangle:
+""" rectangle module """
 
+
+class Rectangle:
+    """ class """
     def __init__(self, width=0, height=0):
+        """ init """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """ width getter """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ width setter """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """ height getter """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ height setter """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
