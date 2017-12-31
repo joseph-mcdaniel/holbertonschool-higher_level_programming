b685990a91d7560e3a7552e94a3f116cba8a0a8b
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-12-31 00:46:43 +0000

    switch attribute order

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,18 +6,6 @@ class Rectangle:
         self.__height = height
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.__width = value
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-
-    @property
     def height(self):
         return self.__height
 
@@ -28,3 +16,15 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
