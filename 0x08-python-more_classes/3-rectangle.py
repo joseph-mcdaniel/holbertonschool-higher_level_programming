ce71891ade8d476b0e2327cb201219fbf2e09b4d
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-01-01 17:07:19 +0000

    change to public instances

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -29,16 +29,16 @@ class Rectangle:
             raise ValueError("height must be >= 0")
 
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         else:
-            return ((self.__width + self.__height) * 2)
+            return ((self.width + self.height) * 2)
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return ("")
 
         return (str("#") * self.__width + "\n") * self.__height
